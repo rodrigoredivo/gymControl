eadff18d694589e444894a67b044643d319aa7a1
RodrigoRedivo <rodrigo.a.redivo@gmail.com>
2020-04-18 12:22:26 -0300

    inserting data into the database

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,16 +1,17 @@
 const Intl = require('intl')
 const { age, date } = require('../../lib/utils')
-
+const db = require('../../config/db')
 
 module.exports = {
   index(req, res) {
+    
     return res.render('instructors/index')
   },
   create(req, res) {
     return res.render('instructors/create')
   },
   show(req, res) {
-    return res.render('instructors/show', { instructor })
+    return res.render('instructors/show')
   },
   post(req, res) {
     const keys = Object.keys(req.body) // CRIANDO ARRAY 
@@ -21,10 +22,33 @@ module.exports = {
       }
     }
 
-    // DESESTRUTURAÇÃO DE OBJETO
-    let { avatar_url, birth, name, services, gender } = req.body
+    const query = `
+      INSERT INTO instructors (
+        name,
+        avatar_url,
+        gender,
+        services,
+        birth,
+        created_at
+      ) VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING id
+    `
 
-    return  
+    const values = [
+      req.body.name,
+      req.body.avatar_url,
+      req.body.gender,
+      req.body.services,
+      date(req.body.birth).iso,
+      date(Date.now()).iso
+    ]
+
+    db.query(query, values, function (err, results) {
+      if(err) return res.send("Database Error!")
+      
+      return res.redirect(`/instructors/${results.rows[0].id}`)
+    })
+    
   },
   edit(req, res) {
     return
